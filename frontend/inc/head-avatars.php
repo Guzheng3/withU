eb4df9f9112a8a21530f0d22b3f84864bb26cc4b
withU-deploy <deploy@withu.local>
2026-08-24 20:53:22 +0800

    refactor: 头像区域对接后台个人资料，昵称动态读取
    - 新增 inc/head-avatars.php 共享组件，10 个页面统一引用
    - 头像从 users.avatar 数据库读取（QQ头像/自定义头像）
    - 昵称从 users.nickname 数据库读取，不再硬编码 Ki./Really
    - 减少 10944 行重复代码

---
+++ frontend/inc/head-avatars.php
@@ -0,0 +1,71 @@
+<div class="bg-wrap central limg" data-avatar-swap="1">
+    <div class="bg-img">
+        <div class="middle Blurkg">
+            <div class="img-male">
+                <div class="avatarArea withu-head-avatar-boy">
+                    <img draggable="false" class="avatarFrame lazy" data-src='https://s1.locimg.com/2024/10/18/db01c99842e69.png' style='transform: scale(1.6);top: 2px;left: 2px;'>
+                    <img draggable="false" class="aiv_touxiang" data-src="<?php echo htmlspecialchars($boyAvatar ?? '/assets/images/default-avatar.svg', ENT_QUOTES, 'UTF-8'); ?>">
+                    <div class="withu-head-avatar-mask">
+                        <div class="withu-head-avatar-top withu-head-avatar-anim-item">
+                            <div class="withu-head-avatar-gender-icon" data-gender="male"><i data-lucide="mars"></i></div>
+                        </div>
+                        <div class="withu-head-avatar-middle withu-head-avatar-anim-item">
+                            <div class="withu-head-avatar-status-text withu-head-avatar-status-away">
+                                <i data-lucide="clock" class="withu-head-avatar-icon-away"></i>
+                                <em>2小时前</em>
+                            </div>
+                            <div class="withu-head-avatar-divider"></div>
+                        </div>
+                        <div class="withu-head-avatar-bottom withu-head-avatar-anim-item">
+                            <div class="withu-head-avatar-location">
+                                <i data-lucide="map-pin"></i>
+                                <em>潘州公园</em>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <span class="shadow-blur"><?php echo htmlspecialchars($boyName ?? '他', ENT_QUOTES, 'UTF-8'); ?></span>
+            </div>
+            <div class="love-icon">
+                <div class="love-info-wrapper">
+                    <div class="distance-bubble" onclick="if(window.WithUMap)WithUMap.open({mode:'lovers'})" style="cursor:pointer">
+                        <div class="distance-icon-box">
+                            <i class="ph-fill ph-navigation-arrow"></i>
+                        </div>
+                        <div class="distance-text">
+                            <span class="distance-text-sm">相距</span>
+                            <span class="km-value">546.9</span>
+                            <span class="distance-text-sm">km</span>
+                        </div>
+                    </div>
+                </div>
+                <img draggable="false" src="../Style/img/like.svg">
+            </div>
+            <div class="img-female">
+                <div class="avatarArea withu-head-avatar-girl">
+                    <img draggable="false" class="avatarFrame lazy" data-src='https://s1.locimg.com/2024/10/18/db01c99842e69.png' style='transform: scale(1.6);top: 2px;left: 2px;'>
+                    <img draggable="false" class="aiv_touxiang" data-src="<?php echo htmlspecialchars($girlAvatar ?? '/assets/images/default-avatar.svg', ENT_QUOTES, 'UTF-8'); ?>">
+                    <div class="withu-head-avatar-mask">
+                        <div class="withu-head-avatar-top withu-head-avatar-anim-item">
+                            <div class="withu-head-avatar-gender-icon" data-gender="female"><i data-lucide="venus"></i></div>
+                        </div>
+                        <div class="withu-head-avatar-middle withu-head-avatar-anim-item">
+                            <div class="withu-head-avatar-status-text withu-head-avatar-status-offline">
+                                <i data-lucide="wifi-off" class="withu-head-avatar-icon-offline"></i>
+                                <em>离线</em>
+                            </div>
+                            <div class="withu-head-avatar-divider"></div>
+                        </div>
+                        <div class="withu-head-avatar-bottom withu-head-avatar-anim-item">
+                            <div class="withu-head-avatar-location">
+                                <i data-lucide="map-pin"></i>
+                                <em>甲子公园</em>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <span class="shadow-blur"><?php echo htmlspecialchars($girlName ?? '她', ENT_QUOTES, 'UTF-8'); ?></span>
+            </div>
+        </div>
+    </div>
+</div>
